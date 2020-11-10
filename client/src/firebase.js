1b4fedf13a4c20cf584b163a4f70f8599ca3d69a
JaronArmiger <jaron.armiger@gmail.com>
2020-11-09 18:54:02 -0600

    create register form

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBB6DlQ00huIdK63Mip6UbTswYJcF0YrUc",
+  authDomain: "beans-fe9bf.firebaseapp.com",
+  databaseURL: "https://beans-fe9bf.firebaseio.com",
+  projectId: "beans-fe9bf",
+  storageBucket: "beans-fe9bf.appspot.com",
+  messagingSenderId: "753344608337",
+  appId: "1:753344608337:web:5a0fee0d58b0a9745ae070"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
