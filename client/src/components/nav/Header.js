6dab6a3e15db67d4060dc965ea42d1a9bee130ca
JaronArmiger <jaron.armiger@gmail.com>
2020-11-09 18:20:01 -0600

    make links in header navigate to corresponding pages

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,29 +1,40 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
 import { 
-  MailOutlined, 
+  UserOutlined, 
   AppstoreOutlined, 
-  SettingOutlined 
+  SettingOutlined ,
+  UserAddOutlined,
 } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState('');
+  const [current, setCurrent] = useState('home');
 
-  const handleClick = () => {
-
+  const handleClick = (e) => {
+    setCurrent(e.key);
   }
 
   return (
     <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Menu.Item key="mail" icon={<MailOutlined />}>
-        Home
-      </Menu.Item>
-      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
+      <Item key="home" icon={<AppstoreOutlined />}>
+        <Link to="/">Home</Link>
+      </Item>
+
+      <Item key="register" icon={<UserAddOutlined />} className="float-right">
+        <Link to="/register">Register</Link>
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+        <Link to="/login">Login</Link>
+      </Item>
+
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
         <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
+          <Item key="setting:1">Option 1</Item>
+          <Item key="setting:2">Option 2</Item>
         </Menu.ItemGroup>
       </SubMenu>
     </Menu>
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
