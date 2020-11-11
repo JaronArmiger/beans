b026dbe08e3f32a18924e456b9f44252a5592c60
JaronArmiger <jaron.armiger@gmail.com>
2020-11-11 17:43:12 -0600

    implement google login

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { auth } from '../../firebase';
+import { auth, googleAuthProvider } from '../../firebase';
 import { toast } from 'react-toastify';
 import { Button } from 'antd';
-import { MailOutlined } from '@ant-design/icons';
+import { MailOutlined, GoogleOutlined } from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 
 const Login = ({ history }) => {
-  const [email, setEmail] = useState('jaron.armiger@gmail.com');
-  const [password, setPassword] = useState('password');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-
   let dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
@@ -32,6 +31,25 @@ const Login = ({ history }) => {
       toast.error(error.message);
       setLoading(false);
     }
+  }
+
+  const googleLogin = async () => {
+    auth.signInWithPopup(googleAuthProvider)
+      .then(async (result) => {
+        const { user } = result;
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+      	  type: 'LOGGED_IN_USER',
+      	  payload: {
+      	    email: user.email,
+      	    token: idTokenResult.token,
+      	  },
+        });
+      })
+      .catch((err) => {
+      	console.log(err);
+      	toast.error(err.message);
+      });
   }
 
   const loginForm = () => (
@@ -74,8 +92,24 @@ const Login = ({ history }) => {
     <div className='container p-5'>
  	  <div className="row">
  	    <div className="col-md-6 offset-md-4">
- 	      <h4>Login</h4>
+ 	      {loading == true ? (
+ 	      	  <h4 className='text-danger'>Loading...</h4>
+ 	      	) : (
+ 	      	  <h4>Login</h4>
+ 	      	)}
  	      {loginForm()}
+
+ 	      <Button
+            onClick={googleLogin}
+            type='danger' // color type
+            className='mb-3'
+            block // to make it stretch horizontally
+            shape='round'
+            icon={<GoogleOutlined />}
+            size='large'
+          >
+      	    Login with Google
+          </Button>
  	    </div>
  	  </div>
     </div>
