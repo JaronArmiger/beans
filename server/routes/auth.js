3a0c37c33de6ad93a422bb659fe6ee1f22bca3ca
JaronArmiger <jaron.armiger@gmail.com>
2020-11-12 19:22:50 -0600

    make auth controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/create-or-update-user', (req, res) => {
-  res.json({
-  	data: 'fuck you bithcs',
-  });
-});
+const { createOrUpdateUser } = require('../controllers/auth');
+
+router.get('/create-or-update-user', createOrUpdateUser);
 
 module.exports = router;
