7a25ef52dd48664ee5bfb256d66af3c13bdca1aa
JaronArmiger <jaron.armiger@gmail.com>
2020-11-12 19:19:12 -0600

    write and import auth route

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/create-or-update-user', (req, res) => {
+  res.json({
+  	data: 'fuck you bithcs',
+  });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { readdirSync } = require('fs');
 require('dotenv').config();
 
 // app
@@ -21,12 +22,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cors());
 
-// route
-app.get('/api', (req, res) => {
-  res.json({
-  	data: 'fuck you',
-  });
-});
+// routes
+readdirSync('./routes').map((r) => {
+  return app.use('/api', require(`./routes/${r}`));
+})
 
 // port
 const port = process.env.PORT || 8000;
