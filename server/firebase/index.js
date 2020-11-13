8a1c4c71dd09781f2aa07662b62235168c1a65aa
JaronArmiger <jaron.armiger@gmail.com>
2020-11-12 19:43:44 -0600

    hook up firebase admin

---
+++ server/firebase/index.js
@@ -0,0 +1,9 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://beans-fe9bf.firebaseio.com"
+});
+
