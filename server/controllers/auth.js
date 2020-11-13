fdeaca904b69f0854dd52d7bd12cfd65b6343c3c
JaronArmiger <jaron.armiger@gmail.com>
2020-11-12 20:23:28 -0600

    implement createOrUpdateUser in auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,22 @@
-exports.createOrUpdateUser = (req, res) => {
-  res.json({
-  	data: 'fuck you bithcs',
-  });
+const User = require('../models/user');
+
+exports.createOrUpdateUser = async (req, res) => {
+  const { email, user_id } = req.user;
+  const user = await User
+    .findOneAndUpdate(
+      { email }, 
+      { name: user_id },
+      { new: true }, // to return updated information
+    )
+
+  if (user) {
+  	console.log('USER UPDATED', user);
+  	res.json(user);
+  } else {
+  	const newUser = await new User({
+  	  email,
+  	}).save();
+  	console.log('USER CREATED', newUser);
+  	res.json({ newUser })
+  }
 }
