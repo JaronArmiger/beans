280f0f3ebd33e387a4e2c409edee8653d1b593b1
JaronArmiger <jaron.armiger@gmail.com>
2020-11-13 18:18:22 -0600

    implement category create

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,7 +1,18 @@
 const Category = require('../models/category');
+const slugify = require('slugify');
 
 exports.create = async (req, res) => {
-  
+  try {
+    const { name } = req.body;
+    const category = await new Category({ 
+      name,
+      slug: slugify(name),
+    }).save();
+    res.json(category);
+  } catch (err) {
+  	console.log(err);
+  	res.status(400).send({ err });
+  }
 };
 
 exports.read = async (req, res) => {
