1cefca0bff0c1d541fd08e678efff04802a28d59
JaronArmiger <jaron.armiger@gmail.com>
2020-11-14 18:55:48 -0600

    create admin nav and import it in AdminDashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import AdminNav from '../../components/nav/AdminNav';
 
 const AdminDashboard = () => {
   return (
-    <React.Fragment>
-      <p>Admin Dashboard</p>
-    </React.Fragment>
+  	<div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">
+          admin dashboard
+        </div>
+      </div>
+  	</div>
   );
 };
 
