4a2e05bea5a5524cab21c766f6bf58470af39533
JaronArmiger <jaron.armiger@gmail.com>
2020-11-16 17:12:16 -0600

    implement file resizing

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Resizer from 'react-image-file-resizer';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const FileUpload = () => {
+  const { token } = useSelector(state => state.user);
+
   const fileUploadAndResize = (e) => {
-    console.log(e.target.files);
+    const files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i], // file
+          720, // maxWidth
+          720, // maxHeigh
+          'JPEG', // compressFormat
+          100, // quality
+          0, // rotation
+          (uri) => { // responseUriFunc
+          	console.log(uri);
+          },
+          'base64', // outputType
+          // minWidth
+          // minHeight
+        );
+      }
+    }
   };
 
   return (
