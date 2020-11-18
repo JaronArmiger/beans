207ec1fc5654dc13191d3843129365060835af71
JaronArmiger <jaron.armiger@gmail.com>
2020-11-18 16:54:17 -0600

    render card titles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getProductsByCount } from '../functions/product';
+import ProductCard from '../components/cards/ProductCard';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -24,9 +26,25 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <p>react home</p>
-    </div>
+  	<React.Fragment>
+  	  <div className='jumbotron'>
+        <h4>All Products</h4>
+        {loading && <LoadingOutlined className='text-warning h1'/>}
+      </div>
+  	  <div className="container">
+        <div className="row">
+          {products.map((product) => {
+          	return (
+          	  <div className="col-md-4" key={product._id}>
+          	    <ProductCard 
+          	     product={product}
+          	    />
+          	  </div>
+          	);
+          })}
+        </div>
+      </div>
+  	</React.Fragment>
   );
 }
 
