b8e0d27ada807f995b20925a111c36572ae70533
JaronArmiger <jaron.armiger@gmail.com>
2020-11-18 17:01:31 -0600

    implement ProductCard

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Card } from 'antd';
+import {
+  EyeOutlined,
+  ShoppingCartOutlined,
+} from '@ant-design/icons';
+import defaultImage from '../../images/snake.jpg';
+import { Link } from 'react-router-dom';
+
+const { Meta } = Card;
 
 const ProductCard = ({ product, }) => {
+  const {
+    title,
+    description,
+    images,
+    slug,
+  } = product;
+
   return (
-    <div>{product.title}</div>
+    <Card
+      cover={
+        (<img 
+            src={(images && images.length > 0) ? images[0].url : defaultImage}
+            style={{ height: '150px', objectFit: 'cover' }}
+            className='p-1'
+            alt={title}
+         />)
+      }
+      actions={[
+        <Link
+          to={`/product/${slug}`}
+        >
+          <EyeOutlined
+            className='text-warning'
+          />
+          <br />
+          View
+        </Link>, 
+        <React.Fragment>
+          <ShoppingCartOutlined 
+            className='text-danger'
+          />
+          <br />
+          Add to Cart
+        </React.Fragment>
+      ]}
+    >
+      <Meta 
+        title={title}
+        description={
+          description ? (
+            (description.length > 30) ?
+            (`${description.substring(0, 30)}...`)
+            : description
+          ) : ''
+        }
+      /> 
+    </Card>
   );
 };
 
