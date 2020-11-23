0bf966ec9a31fdcbd7bd253ad9c7dbfccef43003
JaronArmiger <jaron.armiger@gmail.com>
2020-11-23 17:15:38 -0600

    implement CategoryHome so it displays all products belonging to that category

--- client/src/pages/category/CategoryHome.js
+++ client/src/pages/category/CategoryHome.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getCategory } from '../../functions/category';
 import { Link } from 'react-router-dom';
 import ProductCard from '../../components/cards/ProductCard';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const CategoryHome = ({ match }) => {
   const [category, setCategory] = useState({});
@@ -25,10 +26,27 @@ const CategoryHome = ({ match }) => {
   }, []);
 
   return (
-  	<div> 
-  	{JSON.stringify(category)}
-  	{JSON.stringify(products)}
-  	CategoryHome {slug}
+  	<div className="container-fluid">
+  	  <div className="row">
+  	    <div className="col">
+	  	  <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+	  	    {products.length} products in "{category.name}" category
+	  	  </h4>
+  	    </div>
+  	  </div>
+  	  <div className="row">
+  	    {loading ? (
+	  	    <LoadingOutlined className='text-danger h1' />
+	  	  ) : (
+	  	    products.map((p) => {
+	  	      return (
+	  	      	<div className="col-md-4" key={p._id}>
+	  	      	  <ProductCard product={p} />
+	  	      	</div>
+	  	      );
+	  	    })
+	  	  )}
+  	  </div>
   	</div>
   );
 };
