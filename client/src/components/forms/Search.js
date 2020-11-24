6bc0d2e7fa12dcb43aa6c4d3d9a475d29653e482
JaronArmiger <jaron.armiger@gmail.com>
2020-11-24 16:12:47 -0600

    create search from on front end

---
+++ client/src/components/forms/Search.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { SearchOutlined } from '@ant-design/icons';
+
+export const Search = () => {
+  const dispatch = useDispatch();
+  const { text } = useSelector(state => state.search);
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    dispatch({
+      type: 'SEARCH_QUERY',
+      payload: {
+      	text: e.target.value,
+      }
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`/shop?${text}`);
+  };
+
+  return (
+    <form 
+      className="form-inline my-2 mx-lg-0"
+      onSubmit={handleSubmit}
+    >
+      <input 
+        type="search"
+        className='form-control mr-ms-2'
+        value={text}
+        onChange={handleChange}
+        placeholder='Search'
+      />
+      <SearchOutlined 
+        onClick={handleSubmit}
+        style={{ cursor: 'pointer' }}
+      />
+    </form>
+  );
+};
+
+export default Search;
--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -11,6 +11,7 @@ import { Link } from 'react-router-dom';
 import firebase from 'firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Search from '../forms/Search';
 
 const { SubMenu, Item } = Menu;
 
@@ -82,6 +83,9 @@ const Header = () => {
             </Item>
         </SubMenu>
       )}
+      <span className="float-right p-1">
+        <Search />
+      </span>
     </Menu>
   );
 };
