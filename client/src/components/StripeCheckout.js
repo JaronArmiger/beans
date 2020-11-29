1df6aa3a7d74b3f3610cf3e4e6e7f8323e07b17a
JaronArmiger <jaron.armiger@gmail.com>
2020-11-29 14:16:27 -0600

    implement stripe payment processing

--- client/src/components/StripeCheckout.js
+++ client/src/components/StripeCheckout.js
@@ -8,6 +8,7 @@ import {
   useSelector,
   useDispatch,
 } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import {
   createPaymentIntent,
@@ -54,43 +55,89 @@ const StripeCheckout = ({ history }) => {
   }, []);
 
   const handleChange = async (e) => {
-    
+    // listen for changes in the cart element
+    // and display any errors as the customer types
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : '');
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setProcessing(true);
+    try {
+      const payload = await stripe
+        .confirmCardPayment(clientSecret, {
+        	payment_method: {
+        	  card: elements.getElement(CardElement),
+        	  billing_details: {
+        	  	name: e.target.name.value,
+        	  },
+        	}
+        });
+      if (payload.error) {
+        setError(`Payment failed ${payload.error.message}`);
+        setProcessing(false);
+      } else {
+        // create order and save in database for admin to process
+        // empty user cart from redux store and local storage
+        console.log(payload);
+        setError(null);
+        setProcessing(false);
+        setSucceeded(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
   };
 
   return (
-  	<form 
-  	  id="payment-form"
-  	  className='stripe-form'
-  	  onSubmit={handleSubmit}
-  	>
-      <CardElement 
-        id='card-element'
-        options={cardStyle}
-        onChange={handleChange}
-      />
-      <button 
-        className="stripe-button"
-        disabled={processing || disabled || succeeded}
-      >
-        <span
-          id='button-text'
+    <React.Fragment>
+      <p className={`result-message ${!succeeded ? 'hidden' : ''}`}>
+        Payment Successful!{'   '}
+        <Link to='/user/history'>
+          View in your purchase history
+        </Link>
+      </p>
+    	<form 
+    	  id="payment-form"
+    	  className='stripe-form'
+    	  onSubmit={handleSubmit}
+    	>
+        <CardElement 
+          id='card-element'
+          options={cardStyle}
+          onChange={handleChange}
+        />
+        <button 
+          className="stripe-button"
+          disabled={processing || disabled || succeeded}
         >
-          {processing ? (
-          	<div 
-          	  className='spinner'
-          	  id='spinner'
-          	>
-          	</div>
-          ) : (
-            'Complete Payment'
-          )}
-        </span>
-      </button>
-  	</form>
+          <span
+            id='button-text'
+          >
+            {processing ? (
+            	<div 
+            	  className='spinner'
+            	  id='spinner'
+            	>
+            	</div>
+            ) : (
+              'Complete Payment'
+            )}
+          </span>
+        </button>
+        <br />
+        {error && (
+    	  <div 
+    	    className="card-error text-danger"
+    	    role='alert'
+    	  >
+    	    {error}
+    	  </div>
+    	)}
+    	</form>
+    </React.Fragment>
   );
 };
 
