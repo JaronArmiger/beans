5deeea4a558abd02f4db470152dbc46e45861e45
JaronArmiger <jaron.armiger@gmail.com>
2020-11-28 20:35:38 -0600

    write coupon model

---
+++ server/models/coupon.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const CouponSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    unique: true,
+    uppercase: true,
+    required: 'Name is required',
+    minlength: [6, 'Name must be 6-12 chars'],
+    maxlength: [12, 'Name must be 6-12 chars']
+  },
+  expiry: {
+    type: Date,
+    required: true,
+  },
+  discout: {
+    type: Number,
+    required: true,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Coupon', CouponSchema);
