01cc096f45eaa4cbd194bf5a66128420d38d399d
JaronArmiger <jaron.armiger@gmail.com>
2020-11-30 15:59:55 -0600

    call orders list endpoint in History page

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserNav from '../../components/nav/UserNav';
+import { getUserOrders } from '../../functions/user';
+import { useSelector } from 'react-redux';
+import {
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+} from '@ant-design/icons';
+import { toast } from 'react-toastify';
 
 const History = () => {
+  const [orders, setOrders] = useState([]);
+  const { token } = useSelector(state => state.user);
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+
+  const loadUserOrders = () => {
+    getUserOrders(token)
+      .then(res => {
+        console.log(res.data);
+        setOrders(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
   return (
   	<div className="container-fluid">
       <div className="row">
