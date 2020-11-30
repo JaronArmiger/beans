24f29678aecc268cfb7627bc2c9fb6c4d5f9beac
JaronArmiger <jaron.armiger@gmail.com>
2020-11-30 16:22:27 -0600

    display products in order in user History page

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -27,6 +27,95 @@ const History = () => {
       })
   };
 
+  const showProductInfo = (product, idx) => {
+    const {
+      title,
+      price,
+      color, 
+      count 
+    } = product;
+    const {
+      brand,
+      shipping,
+    } = product.product;
+    return (
+      <tr key={idx}>
+        <td>
+          <b>{title}</b>
+        </td>
+        <td>
+          {price}
+        </td>
+        <td>
+          {brand}
+        </td>
+        <td>
+          {color}
+        </td>
+        <td>
+          {count}
+        </td>
+        <td className='text-center'>
+          {shipping === 'Yes' ?
+            <CheckCircleOutlined className='text-success'/> :
+            <CloseCircleOutlined className='text-danger'/>
+          }
+        </td>
+      </tr>
+    );
+  };
+
+  const showProductsInOrder = (products) => {
+    return (
+      <table className="table table-bordered">
+        <thead className='thead-light'>
+          <tr>
+            <th scope='col'>
+              Title
+            </th>
+            <th scope='col'>
+              Price
+            </th>
+            <th scope='col'>
+              Brand
+            </th>
+            <th scope='col'>
+              Color
+            </th>
+            <th scope='col'>
+              Count
+            </th>
+            <th scope='col'>
+              Shipping
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((p, idx) => (
+            showProductInfo(p, idx)
+          ))}
+        </tbody>
+      </table>
+    )
+};
+
+  const showOrders = () => (
+    orders.map((o, idx) => (
+      <div 
+        className="m-5 p-3 card"
+        key={idx}
+      >
+        <p>show payment info</p>
+        {showProductsInOrder(o.products)}
+        <div className="row">
+          <div className="col">
+            <p>Download PDF</p>
+          </div>
+        </div>
+      </div>
+    ))
+  );
+
   return (
   	<div className="container-fluid">
       <div className="row">
@@ -34,7 +123,13 @@ const History = () => {
           <UserNav />
         </div>
         <div className="col">
-          user history
+          <h4>
+            Your Orders
+          </h4>
+          {orders.length > 0 ? 
+            showOrders() :
+            'No orders yet :\'('
+          }
         </div>
       </div>
   	</div>
