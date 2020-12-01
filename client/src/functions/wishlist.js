123bd4ccd3ef0982c45f98d23d78d219a1917c69
JaronArmiger <jaron.armiger@gmail.com>
2020-11-30 19:33:02 -0600

    write front end functions for wishlist get, add and remove

---
+++ client/src/functions/wishlist.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const getWishlist = async (authtoken) => {
+  return await axios.get(
+    `${process.env.REACT_APP_API}/user/wishlist`,
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const addToWishlist = async (productId, authtoken) => {
+  return await axios.put(
+    `${process.env.REACT_APP_API}/user/wishlist`,
+    { productId },
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const removeProduct = async (productId, authtoken) => {
+  return await axios.delete(
+    `${process.env.REACT_APP_API}/user/wishlist/${productId}`,
+    {
+      headers: {
+        authtoken,
+      },
+    },
+  );
+};
